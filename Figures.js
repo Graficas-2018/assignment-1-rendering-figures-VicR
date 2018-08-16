b192cc2f2167e88c85dbe1ec1098215f7165ec07
Victor Rendon <victor.rs@eucledian.mx>
2018-08-15 23:42:01 -0500

    rhombus

--- Figures.js
+++ Figures.js
@@ -206,8 +206,19 @@ function createTriangle(gl)
 
 function createRhombus(gl)
 {
-    var rhombus = {};
-    return rhombus;
+  var vertexBuffer;
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  var verts = [
+    0.5, 0, 0.0,
+    0, .5, 0.0,
+    0, -.5, 0.0,
+    -0.5, 0, 0.0
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+  var rhombus = {buffer:vertexBuffer, vertSize:3, nVerts:4, primtype:gl.TRIANGLE_STRIP};
+  return rhombus;
 }
 
 function createSphere(gl, radius)
